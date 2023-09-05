5942bf9c96890f632f121ae2b0d0eb84f660edfc
John Miller <55205360+Johnrobmiller@users.noreply.github.com>
2023-09-05 12:10:55 -0600

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+export default function Index() {
+  // !STARTERCONF Here is the main page.
+
+  return <>Hello World!</>;
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+} satisfies Config;
